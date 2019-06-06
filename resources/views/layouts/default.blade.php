7d352e501c16810ff4cd00a7ca0402909b2bc97c
echo <echo@echo.com>
2019-06-06 11:16:44 +0800

    '基础页面'

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title','weibo.app')-自制力控制训练</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+@section('title')
+    about
+    @stop
+@section('content')
+    <h1>about</h1>
+@stop
